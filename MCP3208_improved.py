795d12e528b820204a8638d0285db6d1c6cef7c5
stefyk <43543157+stefyk@users.noreply.github.com>
2020-05-19 17:26:54 +0300

    Update MCP3208_improved.py

--- MCP3208_improved.py
+++ MCP3208_improved.py
@@ -19,7 +19,7 @@ spi.max_speed_hz = 50000
 def read_adc(adc_channel, Vref = 3.3):
 	adc_channel = 0
  	adc = spi.xfer2([6+((4&adc_channel)>>2),(3&adc_channel)<<6,0])
-	data = (adc[1]&3) << 8 to (adc[1]&15) << 8
+	data = ((adc[1]&15) << 8) + adc[2]
  	return data
 
 	# Calculate voltage form ADC value
