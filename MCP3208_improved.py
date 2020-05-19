65be228203eb095dd32a7997227c0a974c085f63
stefyk <43543157+stefyk@users.noreply.github.com>
2020-05-19 17:21:16 +0300

    Update MCP3208_improved.py

--- MCP3208_improved.py
+++ MCP3208_improved.py
@@ -1,5 +1,7 @@
 #This code has been developed, using 
 #https://learn.sparkfun.com/tutorials/python-programming-tutorial-getting-started-with-the-raspberry-pi/experiment-3-spi-and-analog-input
+#This code has been developed, using 
+#https://learn.sparkfun.com/tutorials/python-programming-tutorial-getting-started-with-the-raspberry-pi/experiment-3-spi-and-analog-input
 
 import spidev
 import time
@@ -8,33 +10,37 @@ spi_channel = 0
 	   
 #Enable SPI
 spi = spidev.SpiDev(0, spi_channel)
-spi.max_speed_hz = 100000
+spi.max_speed_hz = 50000
 
 # Hardware SPI configuration (wiring explained in README file):
-SPI_PORT   = 0
-SPI_DEVICE = 0
+#SPI_PORT   = 0
+#SPI_DEVICE = 0
 
+def read_adc(adc_channel, Vref = 3.3):
+	adc_channel = 0
+ 	adc = spi.xfer2([6+((4&adc_channel)>>2),(3&adc_channel)<<6,0])
+ 	data = ((adc[1]&15) << 8) + adc[2]
+ 	return data
+	
+	# Construct single integer out of the reply (2 bytes)
+	adc = 0
+	for n in reply:
+		adc = (adc << 8) + n
 
-def read_adc(channel, Vref = 3.3):
-	 # Make sure ADC channel is 0 or 1
-    if channel != 0:
-       channel = 1
+	# Last bit (0) is not part of ADC value, shift to remove it
+	adc = adc >> 1
+
+	# Calculate voltage form ADC value
+	Voltage = (Vref * adc) / 256
         
- #adc = spi.xfer2([6+((4&channel)>>2),(3&channel)<<6,0])
- #data = ((adc[1]&15) << 8) + adc[2]
-    adc = spi.xfer2([1,(8+channel)<<4,0])
-    data = ((adc[1]&3) << 8) + adc[2]
-    return data
- 
- def Volts(data):
- 	volts = (data * 3.3) / float(4095)
- 	volts = round(volts, 2) # Round off to 2 decimal places
- 	return volts
-  
-		
-while True:
+	return Voltage
+try:			
+	while True:
 		# The read_adc function will get the value of the specified channel (0-1).
-	adc_0 = read_adc(0)
+		adc_0 = read_adc(0)
 		# Print the ADC values.
-	print("The amplitude of V from Ch.0 is:", round(volts, 2),"V")
-	time.sleep(0.000025)
+		print("The amplitude of V from Ch.0 is:", round(adc_0, 2),"V")
+		time.sleep(0.5)
+finally:
+	#closing the SPI channel
+	close()
