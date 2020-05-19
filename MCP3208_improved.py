f0eafb22bf587c076a5081412f638ba920785ac7
stefyk <43543157+stefyk@users.noreply.github.com>
2020-05-19 16:56:33 +0300

    Update MCP3208_improved.py

--- MCP3208_improved.py
+++ MCP3208_improved.py
@@ -15,36 +15,28 @@ SPI_PORT   = 0
 SPI_DEVICE = 0
 
 
-
-def read_adc(adc_channel, Vref = 3.3):
+def read_adc(channel, Vref = 3.3):
 	 # Make sure ADC channel is 0 or 1
-    if adc_channel != 0:
-        adc_channel = 1
-			
-	data = 0b11 # makes it input
-	data = ((data << 1) + adc_channel) << 7 #10-bit number 1100000000
-	data = [data, 0b0000000000] #input 2 words x 10 bits
-	#Performs the SPI transaction and assigns the data to "reply"
-	reply = spi.xfer2(data)   #return reply from input
-	
-	# Construct single integer out of the reply (2 bytes)
-	adc = 0
-	for n in reply: 
-		adc = (adc << 10) + n #for each word it outputs, it will save it in the adc
-
-	# Last bit (0) is not part of ADC value, shift to remove it
-	adc = adc >> 1
-
-	# Calculate voltage form ADC value
-	Voltage = (Vref * adc) / 4096
+    if channel != 0:
+       channel = 1
         
-	return Voltage
+ #adc = spi.xfer2([6+((4&channel)>>2),(3&channel)<<6,0])
+ #data = ((adc[1]&15) << 8) + adc[2]
+ adc = spi.xfer2([1,(8+channel)<<4,0])
+ data = ((adc[1]&3) << 8) + adc[2]
+ return data
+ 
+ def Volts(data):
+  volts = (data * 3.3) / float(4096)
+  volts = round(volts, 2) # Round off to 2 decimal places
+  return volts
+  
 try:			
 	while True:
 		# The read_adc function will get the value of the specified channel (0-1).
 		adc_0 = read_adc(0)
 		# Print the ADC values.
-		print("The amplitude of V from Ch.0 is:", round(adc_0, 2),"V")
+		print("The amplitude of V from Ch.0 is:", round(volts, 2),"V")
 		time.sleep(0.000025)
 finally:
 	#closing the SPI channel
