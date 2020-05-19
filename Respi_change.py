caba475c2d3645b694c46ff794a974ad4f8767a2
stefyk <43543157+stefyk@users.noreply.github.com>
2020-05-19 13:10:19 +0300

    Update Respi_change.py

--- Respi_change.py
+++ Respi_change.py
@@ -19,14 +19,16 @@ SPI_DEVICE = 0
 lasttime=0
  
 def callback_up(channel):
+    sleep(0.005)
     global lasttime
     if lasttime==0:
         lasttime=time.time() #system time saved in lasttime
     else:
         now = time.time() #system time at the moment
         gap=now-lasttime  #print the difference
-        frequency_change=1.0/gap
-        print(frequency_change)
+	print(gap)
+        #frequency_change=1.0/gap
+        #print(frequency_change)
         lasttime=now
     
  
@@ -34,7 +36,7 @@ GPIO.setmode(GPIO.BOARD) #using this wiring mode from library
 SEN = 21  #channel identifier
 GPIO.setup(SEN, GPIO.IN) #setup channel into input mode
 try:
-    GPIO.add_event_detect(SEN, GPIO.RISING, callback=callback_up)
+    GPIO.add_event_detect(SEN, GPIO.RISING, callback=callback_up, bouncetime=5)
     while 1:
         time.sleep(1)
 except KeyboardInterrupt:
