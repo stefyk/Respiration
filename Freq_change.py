0c341e3c6d18a80c4815bc89adda66d34856f5f3
stefyk <43543157+stefyk@users.noreply.github.com>
2020-05-19 16:45:33 +0100

    Add files via upload

---
+++ Freq_change.py
@@ -0,0 +1,52 @@
+import spidev
+import time
+from time import sleep
+import RPi.GPIO as GPIO
+
+spi_channel = 0
+	   
+#Enable SPI
+spi = spidev.SpiDev(0, spi_channel)
+spi.open(0,0)
+spi.max_speed_hz = 50000
+
+# Hardware SPI configuration (wiring explained in README file):
+#SPI_PORT   = 0
+#SPI_DEVICE = 0
+
+#checks whether the GPIO output is detecting rising edge
+#measures the time between two rising edge
+
+lasttime=time.time()
+counter=0
+
+def callback_up(channel):
+
+lasttime = time.time()
+counter = 0 
+lastfreq = 0
+
+def callback_up(channel):
+    global lasttime
+    global counter
+    counter = counter + 1
+    if counter %1000 == 0:
+        now = time.time() #system time at the moment
+        gap=now-lasttime  #print the difference
+        frequency = 1000.0/gap
+        print(frequency - lastfreq)
+        #frequency_change=1.0/gap
+        #print(frequency_change)
+        lasttime=now
+        lastfreq=frequency
+        counter=0
+		
+GPIO.setmode(GPIO.BCM) #using this wiring mode from library
+SEN = 10  #channel identifier
+GPIO.setup(SEN, GPIO.IN) #setup channel into input mode
+try:
+    GPIO.add_event_detect(SEN, GPIO.RISING, callback=callback_up)
+    while 1:
+        time.sleep(1)
+except KeyboardInterrupt:
+   print("Interrupted!")
