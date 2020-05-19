3bb10d036262e1405004a186d6d57b3b159bd1c1
stefyk <43543157+stefyk@users.noreply.github.com>
2020-05-19 17:40:11 +0300

    Update MCP3208_improved.py

--- MCP3208_improved.py
+++ MCP3208_improved.py
@@ -19,20 +19,22 @@ spi.max_speed_hz = 50000
 def read_adc(adc_channel, Vref = 3.3):
 	adc_channel = 0
  	adc = spi.xfer2([6+((4&adc_channel)>>2),(3&adc_channel)<<6,0])
-	data = ((adc[1]&3) << 8) + adc[2]
- 	return data
+	data = ((adc[1]&15) << 8) + adc[2]
+	return data
 
-	# Calculate voltage form ADC value
-	Voltage = (Vref * data) / 4095
-        
-	return Voltage
-try:			
-	while True:
-		# The read_adc function will get the value of the specified channel (0-1).
-		adc_0 = read_adc(0)
-		# Print the ADC values.
-		print("The amplitude of V from Ch.0 is:", round(adc_0, 2),"V")
-		time.sleep(0.5)
-finally:
-	#closing the SPI channel
-	close()
+def ConvertVolts(data,2):
+  volts = (data * 3.3) / float(4096)
+  volts = round(volts,2)
+  return volts
+
+while True:
+ 
+  # Read the voltage data
+  output_volts = ConvertVolts(volts,2)
+ 
+  # Print out results
+  print("The voltage of the channel is: {} ({}V)".format(output_volts,2))
+ 
+  # Wait before repeating loop
+  time.sleep(5)
+
